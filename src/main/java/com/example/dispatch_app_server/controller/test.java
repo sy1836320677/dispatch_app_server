2ef0d0ac9f7d25eb1132cd63ec139fc9939d31fc
孙逸 <1836320677@qq.com>
2022-05-17 03:10:28 +0800

    初试化

--- src/main/java/com/example/dispatch_app_server/controller/test.java
+++ src/main/java/com/example/dispatch_app_server/controller/test.java
@@ -1,4 +1,16 @@
 package com.example.dispatch_app_server.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello")
 public class test {
+    @RequestMapping("/{id}")
+    public String hello(@PathVariable String id){
+        return id;
+    }
+
 }
