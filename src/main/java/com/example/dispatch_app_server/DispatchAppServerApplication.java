43713c1f80cd4a945419e88a1d322d87b0fd2651
孙逸 <1836320677@qq.com>
2022-05-16 22:01:01 +0800

    Initial commit

---
+++ src/main/java/com/example/dispatch_app_server/DispatchAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dispatch_app_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatchAppServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DispatchAppServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dispatch_app_server/DispatchAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dispatch_app_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatchAppServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
