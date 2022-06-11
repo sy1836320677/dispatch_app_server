c895ae0474c38f1f44504d441c224cb8e670fcba
sunyi03 <sunyi03@rd.netease.com>
2022-06-11 19:04:05 +0800

    xml修改

--- src/main/java/com/example/dispatch_app_server/dao/mysql/mapper/LoginRegisterMapper.java
+++ src/main/java/com/example/dispatch_app_server/dao/mysql/mapper/LoginRegisterMapper.java
@@ -2,8 +2,10 @@ package com.example.dispatch_app_server.dao.mysql.mapper;
 
 import com.example.dispatch_app_server.dao.mysql.pojo.LoginRegisterPojo;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
 
 @Mapper
+@Component
 public interface LoginRegisterMapper {
     LoginRegisterPojo findByUsername(String username);
 
